cf80af401295f89a4794f63269f83060f67f88fb
glbn333 <gael.lebrun03@gmail.com>
2024-10-23 21:43:59 +0200

    Training of the Tokenizer

---
+++ tokenizer.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import sentencepiece as spm
+
+# Load the CSV file
+df = pd.read_csv('preprocessed_sequences.csv')
+
+with open('sequences.txt', 'w') as f:
+    for seq in df['sequence']:
+        f.write(seq + '\n')
+
+# Train the SentencePiece tokenizer
+spm.SentencePieceTrainer.train('--input=sequences.txt --model_prefix=amino_acids --vocab_size=5000')
+
+# Load the trained tokenizer
+sp = spm.SentencePieceProcessor()
+sp.Load('amino_acids.model')    
+
+# Tokenize the sequences
+sequence_test = 'MTEYKLVVVGAGGVGKSALTIQLIQNHFVDEYDPTIEDSYRKQVVIDGETCLLDILDTAGQEEYSAMRDQYMRTGEGFLCVFAINNTKSFEDIHQYREQIKRVKDSDDVPMVLVGNKCDLAARTVESRQAQDLARSYGIPYIETSAKTRQGVEDAFYTLVREIRQHKLRKLNPPDESGPGCMNCKCVIS'
+tokens = sp.EncodeAsPieces(sequence_test)
+print(tokens)
+
